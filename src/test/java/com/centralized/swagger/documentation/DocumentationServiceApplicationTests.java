31307139e155cf4c187beac92d63603a13dc4219
MICHAEL PHILOMI <MichaelPhilomi.PaneerSelvam@merrillcorp.com>
2019-03-21 15:59:00 +0530

    Adding first set of source for Document service

---
+++ src/test/java/com/centralized/swagger/documentation/DocumentationServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.centralized.swagger.documentation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DocumentationServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
